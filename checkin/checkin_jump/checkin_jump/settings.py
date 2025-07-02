aaa4ccdadcb9caad985ce030ae9c5ba7583137c2
felipecampelo <felipeaquinocamp@gmail.com>
2025-07-02 18:12:11 -0300

    atualização dump db

--- checkin/checkin_jump/checkin_jump/settings.py
+++ checkin/checkin_jump/checkin_jump/settings.py
@@ -75,12 +75,25 @@ WSGI_APPLICATION = 'checkin_jump.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'checkin_jump',
+         'USER': 'felipecampelo',
+         'PASSWORD': '',
+         'HOST': 'localhost',
+         'PORT': '5432',
+     }
+ }
+
 
 
 # Password validation
