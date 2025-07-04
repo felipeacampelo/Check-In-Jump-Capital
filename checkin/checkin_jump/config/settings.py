ebb4d1f20ebe92054a1c27427408a2ab15f4ec9c
felipecampelo <felipeaquinocamp@gmail.com>
2025-07-04 03:39:43 -0300

    ajustes

--- checkin/checkin_jump/config/settings.py
+++ checkin/checkin_jump/config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,25 +76,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql',
-         'NAME': 'checkin_jump',
-         'USER': 'felipecampelo',
-         'PASSWORD': '',
-         'HOST': 'localhost',
-         'PORT': '5432',
-     }
- }
-
+    'default': dj_database_url.config(
+        default='postgres://felipecampelo:@localhost:5432/checkin_jump'
+    )
+}
 
 
 # Password validation
